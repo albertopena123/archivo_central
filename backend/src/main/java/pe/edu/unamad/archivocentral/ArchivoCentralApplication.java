7ce3fe117bd9eced416319545c904381f1329701
albertopena123 <106981942+albertopena123@users.noreply.github.com>
2024-12-09 09:47:42 -0500

    Initial commit

---
+++ backend/src/main/java/pe/edu/unamad/archivocentral/ArchivoCentralApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unamad.archivocentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivoCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivoCentralApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pe/edu/unamad/archivocentral/ArchivoCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unamad.archivocentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivoCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
